6b5877e593da44a3e5c2eeda59afd1a446aae38b
suchismita93 <149522650+suchismita93@users.noreply.github.com>
2023-11-02 07:15:51 +0530

    Add files via upload

---
+++ python practice.py
@@ -0,0 +1,21 @@
+#generate otp using python
+import random
+#use help() to know about any module/package
+#help(random)
+#we wanted to generate arandom number
+a=random.randint(1,0)
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
