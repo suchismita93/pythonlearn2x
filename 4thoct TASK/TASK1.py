81d8fd3452d87cb90f5367a7163c69993b17fe14
suchismita93 <149522650+suchismita93@users.noreply.github.com>
2023-11-05 07:51:08 +0530

    Add files via upload

---
+++ 4thoct TASK/TASK1.py
@@ -0,0 +1,18 @@
+#TASK 1
+#Print the Name of the user by taking it from the input command.
+user_name=input("Enter your name")
+print(user_name)
+
+
+# TASK 2
+#Take first name and last name of user and print with sep. = "-" and end with /t
+fstname="suchismita"
+lastname="pradhan"
+print(fstname,lastname,sep="-",end="/t")
+print("Allu","Arjun",sep="-",end="/t")
+
+
+#  TASK 3
+#Take a user input as name and say with message You are welcome. "<Pramod>" to the Python Class.
+name=input("Enter user name")
+print("you are welcome",name,"to the python class")
