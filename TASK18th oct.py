b45f15866d09d2e60d02fcc03dfdbee888505e18
suchismita93 <149522650+suchismita93@users.noreply.github.com>
2023-11-13 09:48:29 +0530

    Add files via upload

---
+++ TASK18th oct.py
@@ -0,0 +1,22 @@
+#break to exit a loop when i == 51 while printing the values from 1 to 100
+
+for i in range(1,100):
+    if i==51:
+        break
+    print(i)
+
+r=1
+while r<100:
+    print(r)
+    if r==51:
+        break
+    r=r+1
+
+
+
+
+
+
+
+
+
