3ca91421efcb2552859b4cc92d8575f41b524506
suchismita93 <149522650+suchismita93@users.noreply.github.com>
2023-11-15 00:02:07 +0530

    Add files via upload

---
+++ Task9thoct.py
@@ -0,0 +1,20 @@
+#task
+#Creat a program
+#Take 5 number from users
+#add 1-2 duplicates
+#print the non duplicate number
+num1=int(input("Enter numbers"))
+num2=int(input("Enter numbers"))
+num3=int(input("Enter numbers"))
+num4=int(input("Enter numbers"))
+num5=int(input("Enter numbers"))
+print("numbers :",num1,num2,num3,num4,num5)
+#NON DUPLICATE NUMBER
+res={num1,num2,num3,num4,num5}
+print(res)
+
+
+
+
+
+
